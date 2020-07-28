7c12f632cd3398f1164abec74cb037ee64843a72
itokiana <itokianarajoelison@gmail.com>
2020-07-28 14:10:28 +0300

    First test to real server

--- install.sh
+++ install.sh
@@ -61,11 +61,11 @@ cd $HOME
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
+exec $SHELL &
 
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
+exec $SHELL &
 
 rbenv install $ruby_version
 rbenv global $ruby_version
